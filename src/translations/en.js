019999ac47cc099e70cdc978655d76738f733833
Margiorie Vielma <ruiponche@gmail.com>
2023-09-03 22:34:48 +0200

    change text and fix navbar size

--- src/translations/en.js
+++ src/translations/en.js
@@ -10,7 +10,7 @@ const en={
       tab_panel: {
         "about_p1": "Hello. I am Margiorie Vielma. I am Venezuelan, and I completed my Systems Engineering studies at the University of the Andes. I have been working in web development ever since, in Merida, Montevideo, and Berlin. I started with Python and Django, then moved on to React, MongoDB, and Kubernetes, focusing on various stages of development and collaborating with professionals from various areas.",
         "about_p2": "I have enjoyed all my jobs, both the process and the results: abstracting issues and functionalities, researching technologies, starting from scratch, adding new functionalities, refactoring, assessing the benefit of functionalities, and collaborating with people from different cultures and levels of experience.",
-        "about_p3": "I want to continue developing interesting products, learning, testing concepts, and teaching.",
+        "about_p3": "I want to continue developing interesting products, learning, testing concepts, and sharing knowledge.",
         "about_p4": "If you are interested in working together, write to me.",
         "tecnologies_p1": "Most fequently used.",
         "tecnologies_p2": "Used on previous projects.",
